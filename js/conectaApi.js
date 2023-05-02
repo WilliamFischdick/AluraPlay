574246273a1fa4437528ec42eab37c9a33b6b375
William Fischdick <jwfbit@hotmail.com>
2023-05-01 21:23:14 -0300

    :construction: feat: Implementando a função de adicionar vídeos e buscar pelo nome.

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,3 +1,6 @@
+
+/* Requisição GET*/
+
 async function listaVideos() {
     const conexao = await fetch("http://localhost:3000/videos");
     const conexaoConvertida = await conexao.json();
@@ -5,6 +8,39 @@ async function listaVideos() {
     return conexaoConvertida;
 }
 
+/* Requisição POST */
+
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch ("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
+/* Buscando Vídeos */
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+
+
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo,
+    buscaVideo
 }
